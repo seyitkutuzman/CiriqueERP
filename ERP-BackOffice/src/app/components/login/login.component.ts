f860e8b5b389c2dcfe02543195d0457deabf39d0
Seyit Ahmet Kutuzman <seyit19033247@gmail.com>
2024-07-09 01:55:34 +0300

    launch settings

--- ERP-BackOffice/src/app/components/login/login.component.ts
+++ ERP-BackOffice/src/app/components/login/login.component.ts
@@ -1,12 +1,24 @@
+// login.component.ts
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  loginData = { username: '', password: '' };
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.post('http://your-api-url/api/auth/login', this.loginData)
+      .subscribe(response => {
+        // Do something with the response
+        console.log(response);
+      }, error => {
+        // Handle error
+        console.error(error);
+      });
+  }
 }
