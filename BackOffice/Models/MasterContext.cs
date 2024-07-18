0007c7f65ae7d3b3240dfdd5e5bec35797dc982c
Seyit Ahmet Kutuzman <seyit19033247@gmail.com>
2024-07-18 14:34:35 +0300

    connextion string catalog changed

--- BackOffice/Models/MasterContext.cs
+++ BackOffice/Models/MasterContext.cs
@@ -41,7 +41,7 @@ public partial class MasterContext : DbContext
     public virtual DbSet<Users> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=cirique-erp.database.windows.net;Initial Catalog=master;Persist Security Info=True;User ID=catalcali;password=Seyit2346;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=cirique-erp.database.windows.net;Initial Catalog=CiriqueERP;Persist Security Info=True;User ID=catalcali;password=Seyit2346;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
