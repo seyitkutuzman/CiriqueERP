d9613af3d78e54b2cc7f000f26155c4650e23c45
GoktugKocausu <92365954+GoktugKocausu@users.noreply.github.com>
2024-07-22 15:12:30 +0300

    Create user ekranı için

---
+++ BackOffice/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using BackOffice.Data;
+using BackOffice.Models;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
+namespace BackOffice.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController:ControllerBase
+    {
+        private readonly MasterContext _context;
+        private readonly IConfiguration _configuration;
+
+        public UserController(MasterContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("AddUser")]
+        public IActionResult AddUser([FromBody] Users user)
+        {
+            if (user == null)
+            {
+                return BadRequest("User is null.");
+            }
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok("User added successfully.");
+        }
+
+    }
+}
---
+++ ERP-BackOffice/src/app/components/useradd/useradd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-useradd',
+  standalone: true,
+  imports: [],
+  templateUrl: './useradd.component.html',
+  styleUrl: './useradd.component.css'
+})
+export class UseraddComponent {
+
+}
