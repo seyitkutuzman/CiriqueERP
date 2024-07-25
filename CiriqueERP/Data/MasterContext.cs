d530ec6de39b7d54066ae582cf200bb96107dffc
Seyit Ahmet Kutuzman <seyit19033247@gmail.com>
2024-07-26 01:29:15 +0300

    memoranda of owner backend model created

--- CiriqueERP/Data/MasterContext.cs
+++ CiriqueERP/Data/MasterContext.cs
@@ -41,6 +41,7 @@ public partial class MasterContext : DbContext
     public virtual DbSet<VesselList> VesselList { get; set;}
     public virtual DbSet<CoClass> CoClass { get; set;}
 
+    public virtual DbSet<Mowner> Mowner { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer("Data Source=cirique-erp.database.windows.net;Initial Catalog=CiriqueERP;Persist Security Info=True;User ID=catalcali;password=Seyit2346;Trust Server Certificate=True");
@@ -363,6 +364,71 @@ public partial class MasterContext : DbContext
             entity.HasKey(e => e.ID).HasName("PK__CoClass__3213E83FF541A79F");
 
             entity.ToTable("CoClass");
+
+            entity.Property(e => e.ID)
+                .HasColumnName("id");
+
+            entity.Property(e => e.Description)
+                .HasMaxLength(1000)
+                .IsUnicode(false)
+                .HasColumnName("description");
+
+            entity.Property(e => e.DocNo)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("docNo");
+
+            entity.Property(e => e.OpenedDate)
+                .HasColumnName("openedDate");
+
+            entity.Property(e => e.Status)
+                .HasColumnName("status");
+
+            entity.Property(e => e.Tasks)
+                .HasColumnName("tasks");
+
+            entity.Property(e => e.VesselName)
+                .HasMaxLength(25)
+                .IsUnicode(false)
+                .HasColumnName("vesselName");
+
+            entity.Property(e => e.CompNo)
+                .HasColumnName("compNo");
+
+            entity.Property(e => e.Human)
+                .HasColumnName("human");
+
+            entity.Property(e => e.System)
+                .HasColumnName("system");
+
+            entity.Property(e => e.Material)
+                .HasColumnName("material");
+
+            entity.Property(e => e.Subject)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("subject");
+
+            entity.Property(e => e.DueDate)
+                .HasColumnName("dueDate");
+
+            entity.Property(e => e.ExtendedDate)
+                .HasColumnName("extendedDate");
+
+            entity.Property(e => e.ClosedDate)
+                .HasColumnName("closedDate");
+
+            entity.Property(e => e.Remarks)
+                .HasMaxLength(2000)
+                .IsUnicode(false)
+                .HasColumnName("remarks");
+        });
+
+        modelBuilder.Entity<Mowner>(entity =>
+        {
+            entity.HasKey(e => e.ID).HasName("PK_yeni_tablo");
+
+            entity.ToTable("Mowner");
 
             entity.Property(e => e.ID)
                 .HasColumnName("id");
---
+++ CiriqueERP/Models/Mowner.cs
@@ -0,0 +1,38 @@
+﻿namespace CiriqueERP.Models
+{
+    public class Mowner
+    {
+
+        public int ID { get; set; }
+
+        public string? VesselName { get; set; }
+
+        public string? DocNo { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime OpenedDate { get; set; }
+
+        public int? Status { get; set; }
+
+        public int? Tasks { get; set; }
+
+        public int? CompNo { get; set; }
+
+        public bool? Human { get; set; }
+
+        public bool? System { get; set; }
+
+        public bool? Material { get; set; }
+
+        public string? Subject { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime? ExtendedDate { get; set; }
+
+        public DateTime? ClosedDate { get; set; }
+
+        public string? Remarks { get; set; }
+    }
+}
